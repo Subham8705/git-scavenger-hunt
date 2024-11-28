fafcd91c359cef3df437cbded5192d97e7aba14a
Subham8705 <subhamkumarshee@gmail.com>
2024-11-28 10:49:04 +0530

    cool bug

---
+++ cool.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char input[50];
+
+    printf("Hello! Who are you?\n");
+    scanf("%49s", input);
+
+    printf("Loops are fun!\n");
+
+
+    printf("Well, %s, we hope you are having fun with our git-game!\n", input);
+
+    return 0;
+}
